467292e255f4d57e78b8e670e66314a907ec6055
اسمك <@example.com>
2025-05-31 18:43:32 +0300

    update db config for Render

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,14 +6,14 @@ import { ItunesModule } from './itunes/itunes.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'dpg-d0tgdbs9c44c739f9hj0-a',
+      host: 'dpg-d0tgdbs9c44c739f9hj0-a.oregon-postgres.render.com',
       port: 5432,
       username: 'podcast_fullstack_app_database_user',
-      password: 'nByaxRjkuPWQNTxX1y0wOmLEAQbjJuUw',
+      password: 'nByaxRjkuPWQNTxX1yowOm-LEAQbjJuUw',
       database: 'podcast_fullstack_app_database',
       autoLoadEntities: true,
       synchronize: true,
-    }),
+      }),
     ItunesModule,
   ],
 })
